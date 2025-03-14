d67823078c46402edff073363badb8f3d01373c7
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-03-14 19:21:23 +0300

    feat: implement get_users and log_all_users functions for pdf_diff tool
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/lib/pdf_diff/helpers.rs
+++ src/lib/pdf_diff/helpers.rs
@@ -1,3 +1,26 @@
-pub fn hi() {
-    println!("hello from inside");
+use std::{error::Error, fs::File, io::BufReader};
+use log::{debug, info};
+
+use crate::pdf_diff::models::{Args, User};
+
+pub fn log_all_users(users: &[User]) -> () {
+    println!("length: {:?}", users.len());
+    for user in users.iter() {
+        debug!(
+            "Serving {}, who is watching tables {:?}",
+            user.name, user.watch_tables
+        );
+    }
+}
+
+pub fn get_users(args: &Args) -> Result<Vec<User>, Box<dyn Error>> {
+    //println!("inside get_users {:?}", &args.users_json_path);
+    info!(
+        "Reading users.json from {}",
+        std::fs::canonicalize(&args.users_json_path)?.display()
+    );
+    let users_file = BufReader::new(File::open(&args.users_json_path)?);
+    let users: Vec<User> = serde_json::from_reader(users_file)?;
+    log_all_users(&users);
+    Ok(users)
 }
