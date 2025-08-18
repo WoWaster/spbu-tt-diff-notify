da372a49ad9fa04b3a8c60fc6ee8d6fea22ca3a7
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-08-18 18:15:17 +0300

    feat: first pack of tests
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/lib/tt_diff/helpers.rs
+++ src/lib/tt_diff/helpers.rs
@@ -255,3 +255,246 @@ pub fn write_previous_events(
 
     Ok(serde_json::to_writer_pretty(events_file, &events)?)
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        collections::{HashMap, HashSet},
+        path::PathBuf,
+    };
+
+    use crate::tt_diff::{
+        helpers::{find_diffs_in_events, get_previous_events, get_users},
+        models::{
+            educator_model::{
+                ContingentUnitName, DayStudyEvent, EducatorDay, EducatorEvents, EventLocation,
+            },
+            Args,
+        },
+    };
+
+    use super::*;
+
+    fn make_event(
+        time: &str,
+        subject: &str,
+        dates: Vec<&str>,
+        locations: Vec<&str>,
+        groups: Vec<&str>,
+    ) -> DayStudyEvent {
+        DayStudyEvent {
+            time_interval_string: time.to_string(),
+            subject: subject.to_string(),
+            dates: dates.into_iter().map(|d| d.to_string()).collect(),
+            event_locations: locations
+                .into_iter()
+                .map(|loc| EventLocation {
+                    display_name: loc.to_string(),
+                })
+                .collect(),
+            contingent_unit_names: groups
+                .into_iter()
+                .map(|g| ContingentUnitName {
+                    item1: "Группа".to_string(),
+                    item2: g.to_string(),
+                })
+                .collect(),
+        }
+    }
+
+    #[test]
+    fn get_users_valid_json() {
+        let args = Args {
+            users_json_path: PathBuf::from("tests/test.users.json"),
+            config_json_path: PathBuf::from("tests/test.config.json"),
+            previous_events_json_path: PathBuf::from("tests/test.less_events.json"),
+        };
+
+        let users = get_users(&args).unwrap();
+        let watch_ed_ref = HashSet::from([1928, 1879]);
+        assert_eq!(users.len(), 1);
+        assert_eq!(users[0].name, "Энди Уорхол");
+        assert_eq!(users[0].watch_educators, watch_ed_ref);
+        assert!(users[0].watch_groups.is_empty());
+        assert_eq!(users[0].email, "campbellsoupthebest@gmail.com");
+    }
+
+    #[test]
+    fn get_prev_events_correct_json() {
+        let args = Args {
+            users_json_path: PathBuf::from("tests/test.users.json"),
+            config_json_path: PathBuf::from("tests/test.config.json"),
+            previous_events_json_path: PathBuf::from("tests/test.less_events.json"),
+        };
+
+        let prev_ev = get_previous_events(&args).unwrap();
+
+        let mut prev_ev_ref: HashMap<u32, EducatorEvents> = HashMap::new();
+
+        let warhol = EducatorEvents {
+            educator_long_display_text: "Энди Уорхол".to_string(),
+            educator_master_id: 1928,
+            educator_events_days: vec![EducatorDay {
+                day_string: "Понедельник".to_string(),
+                day_study_events_count: 1,
+                day_study_events: vec![DayStudyEvent {
+                    time_interval_string: "08:30-10:00".to_string(),
+                    subject: "Как превратить искусство в массовый продукт".to_string(),
+                    dates: vec!["01.09.1963".to_string()],
+                    event_locations: vec![EventLocation {
+                        display_name: "231 East 47th Street".to_string(),
+                    }],
+                    contingent_unit_names: vec![ContingentUnitName {
+                        item1: "Группа".to_string(),
+                        item2: "101A".to_string(),
+                    }],
+                }],
+            }],
+        };
+        prev_ev_ref.insert(1928, warhol);
+
+        let malevich = EducatorEvents {
+            educator_long_display_text: "Казимир Малевич".to_string(),
+            educator_master_id: 1879,
+            educator_events_days: vec![EducatorDay {
+                day_string: "Вторник".to_string(),
+                day_study_events_count: 2,
+                day_study_events: vec![DayStudyEvent {
+                    time_interval_string: "09:00-10:30".to_string(),
+                    subject: "От кубизма к супрематизму".to_string(),
+                    dates: vec!["29.12.1915".to_string()],
+                    event_locations: vec![EventLocation {
+                        display_name: "Дворцовая площадь, д. 6/8".to_string(),
+                    }],
+                    contingent_unit_names: vec![
+                        ContingentUnitName {
+                            item1: "Группа".to_string(),
+                            item2: "201A".to_string(),
+                        },
+                        ContingentUnitName {
+                            item1: "Группа".to_string(),
+                            item2: "201B".to_string(),
+                        },
+                    ],
+                }],
+            }],
+        };
+        prev_ev_ref.insert(1879, malevich);
+        assert_eq!(prev_ev, prev_ev_ref);
+    }
+
+    #[test]
+    fn find_diffs_in_events1() {
+        let args_less = Args {
+            users_json_path: PathBuf::from("tests/test.users.json"),
+            config_json_path: PathBuf::from("tests/test.config.json"),
+            previous_events_json_path: PathBuf::from("tests/test.less_events.json"),
+        };
+        let args_many = Args {
+            users_json_path: PathBuf::from("tests/test.users.json"),
+            config_json_path: PathBuf::from("tests/test.config.json"),
+            previous_events_json_path: PathBuf::from("tests/test.many_events.json"),
+        };
+        let educators_old = get_previous_events(&args_less).unwrap();
+        let educators_new = get_previous_events(&args_many).unwrap();
+        let diff = find_diffs_in_events(&educators_old, &educators_new).unwrap();
+        // TODO: unreadable? prob insert it after formatting tests
+        assert_eq!(diff.get(&1928).unwrap().1, "@@ -4,13 +4,14 @@\n   \"EducatorEventsDays\": [\n     {\n       \"DayString\": \"Понедельник\",\n-      \"DayStudyEventsCount\": 1,\n+      \"DayStudyEventsCount\": 2,\n       \"DayStudyEvents\": [\n         {\n           \"TimeIntervalString\": \"08:30-10:00\",\n           \"Subject\": \"Как превратить искусство в массовый продукт\",\n           \"Dates\": [\n-            \"01.09.1963\"\n+            \"01.09.1963\",\n+            \"08.09.1963\"\n           ],\n           \"EventLocations\": [\n             {\n@@ -21,6 +22,54 @@\n             {\n               \"Item1\": \"Группа\",\n               \"Item2\": \"101A\"\n+            },\n+            {\n+              \"Item1\": \"Группа\",\n+              \"Item2\": \"101B\"\n+            }\n+          ]\n+        },\n+        {\n+          \"TimeIntervalString\": \"10:15-11:45\",\n+          \"Subject\": \"Истоки поп-арта\",\n+          \"Dates\": [\n+            \"01.09.1968\",\n+            \"08.09.1968\"\n+          ],\n+          \"EventLocations\": [\n+            {\n+              \"DisplayName\": \"33 Union Square West\"\n+            }\n+          ],\n+          \"ContingentUnitNames\": [\n+            {\n+              \"Item1\": \"Группа\",\n+              \"Item2\": \"102B\"\n+            }\n+          ]\n+        }\n+      ]\n+    },\n+    {\n+      \"DayString\": \"Среда\",\n+      \"DayStudyEventsCount\": 1,\n+      \"DayStudyEvents\": [\n+        {\n+          \"TimeIntervalString\": \"13:00-14:30\",\n+          \"Subject\": \"Истоки поп-арта\",\n+          \"Dates\": [\n+            \"02.09.1968\",\n+            \"10.09.1968\"\n+          ],\n+          \"EventLocations\": [\n+            {\n+              \"DisplayName\": \"33 Union Square West\"\n+            }\n+          ],\n+          \"ContingentUnitNames\": [\n+            {\n+              \"Item1\": \"Группа\",\n+              \"Item2\": \"103C\"\n             }\n           ]\n         }\n");
+        assert_eq!(diff.get(&1879).unwrap().1, "@@ -10,6 +10,7 @@\n           \"TimeIntervalString\": \"09:00-10:30\",\n           \"Subject\": \"От кубизма к супрематизму\",\n           \"Dates\": [\n+            \"22.12.1915\",\n             \"29.12.1915\"\n           ],\n           \"EventLocations\": [\n@@ -27,6 +28,25 @@\n               \"Item2\": \"201B\"\n             }\n           ]\n+        },\n+        {\n+          \"TimeIntervalString\": \"11:00-12:30\",\n+          \"Subject\": \"Декларация прав художника\",\n+          \"Dates\": [\n+            \"15.08.1918\",\n+            \"22.08.1918\"\n+          ],\n+          \"EventLocations\": [\n+            {\n+              \"DisplayName\": \"Дворцовая площадь, д. 6/8\"\n+            }\n+          ],\n+          \"ContingentUnitNames\": [\n+            {\n+              \"Item1\": \"Группа\",\n+              \"Item2\": \"202A\"\n+            }\n+          ]\n         }\n       ]\n     }\n");
+    }
+
+    #[test]
+    fn format_event_as_string1() {
+        let test_event = DayStudyEvent {
+            time_interval_string: "09:30-11.00".to_string(),
+            subject: "Матлогика".to_string(),
+            dates: vec!["01.09.2025".to_string()],
+            event_locations: vec![EventLocation {
+                display_name: "Университетский пр. 28Д".to_string(),
+            }],
+            contingent_unit_names: vec![ContingentUnitName {
+                item1: "Группа".to_string(),
+                item2: "23.Б15-мм".to_string(),
+            }],
+        };
+        let formatted_event = format_event_as_string(&test_event);
+        assert_eq!(formatted_event, "    <b>Предмет:</b> Матлогика<br>    <b>Время:</b> 09:30-11.00<br>    <b>Даты:</b> 01.09.2025<br>    <b>Места:</b> Университетский пр. 28Д<br>    <b>Направления:</b> Группа 23.Б15-мм<br>")
+    }
+
+    #[test]
+    fn vec_eq_unordered_equal() {
+        let fst = [1, 2, 3];
+        assert!(vec_eq_unordered(&fst, &fst))
+    }
+
+    #[test]
+    fn vec_eq_unordered_mixed_order() {
+        let fst = [3, 1, 2];
+        let snd = [1, 2, 3];
+        assert!(vec_eq_unordered(&fst, &snd))
+    }
+
+    #[test]
+    fn vec_eq_unordered_unequal() {
+        let fst = [3, 3, 2];
+        let snd = [1, 2, 3];
+        assert!(!vec_eq_unordered(&fst, &snd))
+    }
+
+    #[test]
+    fn vec_eq_unordered_dif_length() {
+        let fst = [3, 3];
+        let snd = [1, 2, 3];
+        assert!(!vec_eq_unordered(&fst, &snd))
+    }
+
+    #[test]
+    fn event_eq_equal() {
+        let test_event = make_event(
+            "09:30-11.00",
+            "Матлогика",
+            vec!["01.09.2025", "03.09.2025"],
+            vec!["Университетский пр. 28Д", "Менделеевская л. 2"],
+            vec!["23.Б15-мм", "23.Б11-мм"],
+        );
+        assert!(event_eq(&test_event, &test_event))
+    }
+
+    #[test]
+    fn event_eq_mixed_order() {
+        let old = make_event(
+            "09:30-11.00",
+            "Матлогика",
+            vec!["01.09.2025", "03.09.2025"],
+            vec!["Университетский пр. 28Д", "Менделеевская л. 2"],
+            vec!["23.Б15-мм", "23.Б11-мм"],
+        );
+        let new = make_event(
+            "09:30-11.00",
+            "Матлогика",
+            vec!["03.09.2025", "01.09.2025"],
+            vec!["Менделеевская л. 2", "Университетский пр. 28Д"],
+            vec!["23.Б11-мм", "23.Б15-мм"],
+        );
+        assert!(event_eq(&old, &new))
+    }
+
+    #[test]
+    fn event_eq_unequal() {
+        let old = make_event(
+            "09:30-11.00",
+            "Матлогика",
+            vec!["01.09.2025", "03.09.2025"],
+            vec!["Университетский пр. 28Д", "Менделеевская л. 2"],
+            vec!["23.Б15-мм", "23.Б11-мм"],
+        );
+        let new = make_event(
+            "09:30-11.00",
+            "Матлогика",
+            vec!["03.09.2025", "01.09.2025"],
+            vec!["Менделеевская л. 2", "Университетский пр. 28Д"],
+            vec!["23.Б11-мм"],
+        );
+        assert!(!event_eq(&old, &new))
+    }
+}
