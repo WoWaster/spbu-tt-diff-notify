bdc04cac5c0373d2c29a77010a96a39772022ba6
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2025-09-11 23:33:57 +0300

    ref: minor refactoring due to replacing run into lib
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- src/bin/tt_diff.rs
+++ src/bin/tt_diff.rs
@@ -1,39 +1,19 @@
 use lib::tt_diff::helpers;
-use lib::tt_diff::mail_sender::LetterSender;
 use lib::tt_diff::models;
-use lib::tt_diff::schedule_getter::ScheduleGetter;
+use lib::tt_diff::run_tool::run;
 
 use clap::Parser;
 use figment::{
     providers::{Env, Format, Json},
     Figment,
 };
-use helpers::{generate_diff_messages, get_previous_events, get_users, write_previous_events};
+use helpers::write_previous_events;
 use lettre::{
     transport::smtp::authentication::{Credentials, Mechanism},
     SmtpTransport,
 };
 use log::info;
 use models::{Args, Config};
-
-async fn run<SG: ScheduleGetter, LS: LetterSender>(
-    schedule_getter: SG,
-    letter_sender: LS,
-    args: Args,
-    config: Config,
-) {
-    let users = get_users(&args).unwrap();
-    let educator_events_old = get_previous_events(&args).unwrap();
-    info!("Found {} educators in db", educator_events_old.len());
-    let educator_events_new = schedule_getter.get_schedule(&users).await;
-    let educators_changed = generate_diff_messages(&educator_events_old, &educator_events_new);
-    info!(
-        "Found {} changed educators schedules",
-        educators_changed.len()
-    );
-    letter_sender.form_and_send_letters(users, config, educators_changed);
-    write_previous_events(&args, educator_events_new).unwrap();
-}
 
 #[tokio::main]
 async fn main() {
@@ -63,6 +43,6 @@ async fn main() {
         ))
         .authentication(vec![Mechanism::Plain])
         .build();
-    // is async-await mechanic here really necessary?
-    run(http_client, sender, args, config).await;
+    let new_events = run(http_client, sender, &args, config).await;
+    write_previous_events(&args, new_events).unwrap();
 }
